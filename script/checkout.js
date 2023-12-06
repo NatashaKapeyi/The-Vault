ab62eabd3327b1c1fb37e09b1e5af059a3bbd96a
NatashaKapeyi <natashazikhona7@gmail.com>
2023-12-06 15:16:12 +0200

    battery low

--- script/checkout.js
+++ script/checkout.js
@@ -1,3 +1,35 @@
 //checkout
 // date at the footer for checkout
 document.querySelector('#NewDate').textContent= new Date().getUTCFullYear()
+
+// table 
+
+// let cart= document.querySelector('[data-product-cart]')
+// cart.innerHTML=`<table class="table table-borderless table-danger">
+// <thead>
+//   <tr>
+//     <th scope="col">#</th>
+//     <th scope="col">DESCRIPTION</th>
+//     <th scope="col">NAME</th>
+//     <th scope="col">PRICE/AMOUNT</th>
+//   </tr>
+// </thead>
+// <tbody>
+//   <tr>
+//     <th scope="row">1</th>
+//     <td></td>
+//     <td></td>
+//     <td></td>
+//   </tr>
+//   <tr>
+//     <th scope="row">2</th>
+//     <td></td>
+//     <td></td>
+//     <td></td>
+//   </tr>
+//   <tr>
+    
+
+// </tbody>
+// </table>`;
+
