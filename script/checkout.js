f586768da6fae1e18ed0c1a02fda0c364e3f5223
NatashaKapeyi <natashazikhona7@gmail.com>
2023-12-07 15:27:40 +0200

    fixed a lot

--- script/checkout.js
+++ script/checkout.js
@@ -1,29 +1,58 @@
 //checkout
 // date at the footer for checkout
 document.querySelector('#NewDate').textContent= new Date().getUTCFullYear()
-
+let cart = JSON.parse(localStorage.getItem("basket"))
 //
+
 let finalSelection = document.querySelector('[data-product-cart]')
-let cData = JSON.parse(localStorage.getItem('basket'))
+// let cData = JSON.parse(localStorage.getItem('basket'))
+     let cData= Object.groupBy( cart, (product) => product.id)
+
+
 
 function displaycData() {
-    finalSelection.innerHTML = ""
-    cData.map(product=>{
+    finalSelection.innerHTML = "";
+    if(cData)
+    for( let key in cData){
         finalSelection.innerHTML += `
          <div class= table-responsive>
-        <table class="table table-borderles table-sm table-align-middle">
+        <table class="table table-borderless table-hover table-sm text-center ">
   
         <tbody>
         <tr>
-        <th scope="">${product.id}</th>
-        <td><img src="${product.img}" class="w-25 img-fluid" alt=""</td>
-        <td class="text-black">${product.name}</td>
-        <td class="text-black">R${product.amount}</td>
+        <th scope="row" class="w-25 text-start">${cData[key][0].id}</th>
+        <td class="w-25"><img src="${cData[key][0].img}" class="w-50 justify-content-center img-fluid" alt=""</td>
+        <td class="text-black w-25">${cData[key][0].name}</td>
+        <td class="text-black w-25">R${cData[key][0].amount}</td>
+        <td class="text-black w-25">${cData[key][0].length}</td>
         </tr>
         <tbody>
         </table>
         </div>
         `
-    })
+    }
 }
 displaycData()
+
+
+//amount due
+let amount =document.querySelector('[data-product-amount]')
+
+function amountDue(){
+
+    
+}
+
+
+//deleting
+let deleteBtn= document.querySelector("[data-product-delete]")
+deleteBtn.addEventListener("click",removeItem);
+
+
+function removeItem(){
+  let remove = document.querySelector('[data-product-cart]');
+  remove.remove();
+  localStorage.removeItem("basket");
+
+}
+  
