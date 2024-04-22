13870b6d4e5a7a2986f53e48e6634362ac2dd2d4
NatashaKapeyi <natashazikhona7@gmail>
2024-04-22 16:28:21 +0200

    updated

--- script/product.js
+++ script/product.js
@@ -10,32 +10,32 @@ localStorage.setItem('products',JSON.stringify([
         "id":"1",
         "name":"CHECKERBOARD SKATE SLIP-ON",
         "amount":"999",
-        "img":"https://i.postimg.cc/wTVDgf3V/421925-5088-529124-3-600x-removebg-preview-3.png",     
+        "img":"https://i.ibb.co/wS4h4hd/421925-5088-529124-3-600x-removebg-preview-2.png",     
     },
     {
         "id":"2",
         "name":"CLASSIC SLIP-ON 98 DX SHOES",
         "amount":"1399",
-        "img":"https://i.postimg.cc/52ygGdpv/431103-3920-301134-3-600x-removebg-preview-1.png",
+        "img":"https://i.ibb.co/4Ys97Dk/431103-3920-301134-3-600x-removebg-preview.png",
     },
     {
         "id":"3",
         "name":"HARBOR MULE VR3",
         "amount":"1799",
-        "img":"https://i.postimg.cc/2SFgxfcD/431877-1323-778134-2-600x-removebg-preview.png",
+        "img":"https://i.ibb.co/6PMt7b0/431877-1323-778134-2-600x-removebg-preview.png",
     },
     {
         "id":"4",
         "name":"CHECKERBOARD SK8-HI STACKED SHOES",
         "amount":"1399",
-        "img":"https://i.postimg.cc/mgnvpS3L/429088-6676-880924-2-600x-removebg-preview-2.png",
+        "img":"https://i.ibb.co/3y5CXX3/429088-6676-880924-2-600x-removebg-preview-2.png",
     },
    
     {
         "id":"5",
         "name":"Vans Mega Check Knu Skool Skate Shoes",
         "amount":" 2322",
-        "img":"https://i.postimg.cc/htqHxG0s/s-l1600-removebg-preview.png",
+        "img":"https://i.ibb.co/q7CnDCf/s-l1600-removebg-preview.png",
     },
    
 ]));
